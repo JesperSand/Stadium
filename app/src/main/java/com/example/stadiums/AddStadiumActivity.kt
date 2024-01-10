686fccea930121a7742b471b3bc1cac2b962a6be
Jesper Sand <jespersand@jespers-air.home>
2024-01-11 00:57:03 +0100

    fixed so user can now upload images into firestore

--- app/src/main/java/com/example/stadiums/AddStadiumActivity.kt
+++ app/src/main/java/com/example/stadiums/AddStadiumActivity.kt
@@ -3,6 +3,7 @@ package com.example.stadiums
 import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.net.Uri
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -13,19 +14,22 @@ import android.provider.MediaStore
 import android.util.Log
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.FirebaseStorage
 import java.text.NumberFormat
+import java.util.UUID
 
 class AddStadiumActivity : AppCompatActivity() {
 
     private val REQUEST_CODE = 1
     lateinit var imageViewStadium: ImageView
+    var selectedImage: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_stadium)
         window.decorView.systemUiVisibility =
-        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
         val db = Firebase.firestore
 
@@ -39,6 +43,7 @@ class AddStadiumActivity : AppCompatActivity() {
         this.imageViewStadium = findViewById(R.id.imageViewStadium)
         val buttonAdd = findViewById<Button>(R.id.buttonAdd)
 
+
         buttonUploadImage.setOnClickListener {
             val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(pickPhoto, REQUEST_CODE)
@@ -46,6 +51,8 @@ class AddStadiumActivity : AppCompatActivity() {
 
         buttonAdd.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
+                buttonAdd.isEnabled = false  // Inaktivera knappen
+                Log.d(TAG, "Button clicked!")
                 val name = editTextStadiumName.text.toString()
                 val country = editTextCountry.text.toString()
                 val city = editTextCity.text.toString()
@@ -58,37 +65,57 @@ class AddStadiumActivity : AppCompatActivity() {
                 } catch (e: NumberFormatException) {
                     // Visa ett felmeddelande till användaren
                 }
+                val filename = UUID.randomUUID().toString()
+                val storageRef = FirebaseStorage.getInstance().getReference("/images/$filename")
 
-                val stadium = hashMapOf(
-                    "name" to name,
-                    "country" to country,
-                    "city" to city,
-                    "built" to built,
-                    "team" to team,
-                    "capacity" to formattedCapacity
-                )
+                if (selectedImage != null) {
+                    storageRef.putFile(selectedImage!!)
+                        .addOnSuccessListener {
+                            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                                val imageUrl = uri.toString()
 
-                // Din kod...
-                // I AddStadiumActivity, när du har lagt till den nya stadion
-                db.collection("stadiums")
-                    .add(stadium)
-                    .addOnSuccessListener { documentReference ->
-                        Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-                        setResult(Activity.RESULT_OK)  // Sätt resultatet till OK
-                        finish()  // Avsluta aktiviteten
-                    }
-                    .addOnFailureListener { e ->
-                        Log.w(TAG, "Error adding document", e)
-                    }
+                                val stadium = hashMapOf(
+                                    "name" to name,
+                                    "country" to country,
+                                    "city" to city,
+                                    "built" to built,
+                                    "team" to team,
+                                    "capacity" to formattedCapacity,
+                                    "imageUrl" to imageUrl  // Lägg till imageUrl i din map
+                                )
+
+                                db.collection("stadiums")
+                                    .add(stadium)
+                                    .addOnSuccessListener { documentReference ->
+                                        Log.d(
+                                            TAG,
+                                            "DocumentSnapshot added with ID: ${documentReference.id}"
+                                        )
+                                        setResult(Activity.RESULT_OK)  // Sätt resultatet till OK
+                                        finish()  // Avsluta aktiviteten
+                                    }
+                                    .addOnFailureListener { e ->
+                                        Log.w(TAG, "Error adding document", e)
+                                    }
+                            }
+                        }
+                        .addOnFailureListener {
+                            // Här kan du hantera vad som ska hända om uppladdningen misslyckas
+                            buttonAdd.isEnabled = true  // Aktivera knappen igen
+                        }
+                } else {
+                    buttonAdd.isEnabled = true  // Aktivera knappen igen
+                }
+
             }
-        })
 
+        })
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-            val selectedImage = data.data
+            selectedImage = data.data
             // Gör något med selectedImage, till exempel sätt det i din ImageView
             Log.d("ImageHandling", "Selected image: $selectedImage")
             imageViewStadium.setImageURI(selectedImage)
